0c4ddb0c143e2aec2127669e6f582bd2e83a6afd
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2024-02-10 15:59:09 +0530

    Add edit and delete functionality to TodoList component

--- component/TodoList.jsx
+++ component/TodoList.jsx
@@ -3,12 +3,32 @@ import { useState } from "react";
 function TodoList() {
   const [todos, setTodos] = useState([]);
   const [inputValue, setInputValue] = useState("");
+  const [editIndex, setEditIndex] = useState(null);
 
   const handleAddTodo = () => {
     if (inputValue.trim() !== "") {
-      setTodos([...todos, inputValue]);
+      if (editIndex !== null) {
+        // If editIndex is not null, it means we're editing an existing todo
+        const updatedTodos = [...todos];
+        updatedTodos[editIndex] = inputValue;
+        setTodos(updatedTodos);
+        setEditIndex(null);
+      } else {
+        // Otherwise, we're adding a new todo
+        setTodos([...todos, inputValue]);
+      }
       setInputValue("");
     }
+  };
+
+  const handleEditTodo = (index) => {
+    setInputValue(todos[index]);
+    setEditIndex(index);
+  };
+
+  const handleDeleteTodo = (index) => {
+    const updatedTodos = todos.filter((_, i) => i !== index);
+    setTodos(updatedTodos);
   };
 
   const handleInputChange = (event) => {
@@ -24,10 +44,16 @@ function TodoList() {
         value={inputValue}
         onChange={handleInputChange}
       />
-      <button onClick={handleAddTodo}>Add</button>
+      <button onClick={handleAddTodo}>
+        {editIndex !== null ? "Update" : "Add"}
+      </button>
       <ul>
         {todos.map((todo, index) => (
-          <li key={index}>{todo}</li>
+          <li key={index}>
+            {todo}
+            <button onClick={() => handleEditTodo(index)}>Edit</button>
+            <button onClick={() => handleDeleteTodo(index)}>Delete</button>
+          </li>
         ))}
       </ul>
     </div>
